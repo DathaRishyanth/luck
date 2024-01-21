d90eaf1cb61dab122da252b0469f4f8cbf1f55c4
DathaRishyanth <154327632+DathaRishyanth@users.noreply.github.com>
2024-01-21 19:43:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var audio = new Audio("Anime Sound Girl Ahh Sound Background Sound Effects Baba Sound Effects.mp3");
+
+document.querySelector("button").addEventListener("click",play);
+
+function play(){
+    audio.play();
+}
+
